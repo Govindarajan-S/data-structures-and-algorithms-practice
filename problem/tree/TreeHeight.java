6bbb0f1636be9d5438b9f3e160cfe82b999aa64e
Govindarjan Sivaraj <sgovind2112@gmail.com>
2020-09-14 13:12:01 +0530

    Implement Height of tree and level order traversal

---
+++ problem/tree/TreeHeight.java
@@ -0,0 +1,65 @@
+package problem.tree;
+
+import java.util.Scanner;
+
+class Node {
+    Node left;
+    Node right;
+    int data;
+    
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class TreeHeight {
+
+	/*
+    class Node 
+    	int data;
+    	Node left;
+    	Node right;
+	*/
+	public static int height(Node root) {
+      	// Write your code here.
+        int height = 0;
+        if (root == null || (root.left == null && root.right == null)) {
+            return height;
+        }
+        int leftHeight = 1 + height(root.left);
+        int rightHeight = 1 + height(root.right);
+        height = Math.max(leftHeight, rightHeight);
+        return height;
+    }
+
+	public static Node insert(Node root, int data) {
+        if(root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if(data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t = scan.nextInt();
+        Node root = null;
+        while(t-- > 0) {
+            int data = scan.nextInt();
+            root = insert(root, data);
+        }
+        scan.close();
+        int height = height(root);
+        System.out.println(height);
+    }	
+}
