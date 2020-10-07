74c3f337bb4511fb096721fe8dca20ac8bef9e28
Govindarjan Sivaraj <sgovind2112@gmail.com>
2020-10-07 11:48:05 +0530

    Implement insert, prepend and pop

--- problem/arrays/ResizeArray.java
+++ problem/arrays/ResizeArray.java
@@ -43,11 +43,48 @@ public class ResizeArray {
         }
         return newArr;
     }
+
+    public void insert(int index, int value) {
+        if (size == capacity) {
+            arr = resize(capacity * 2);
+        }
+        for (int i = size - 1; i >= index; i--) {
+            arr[i + 1] = arr[i];
+        }
+        arr[index] = value;
+        size++;
+    }
+
+    public void prepend(int value) {
+        insert(0, value);
+    }
     
+    public int pop() {
+        if (isEmpty()) {
+            throw new RuntimeException("Array is Empty!");
+        }
+        int removedValue = arr[size - 1];
+        if (size == capacity / 4) {
+            arr = resize(capacity / 2);
+        }
+        size--;
+        return removedValue;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
     public static void main(String[] args) {
         testGetWithinRange();
         testGetLessThanZero();
         testGetGreaterThanSize();
+        testInsertWhenNotFull();
+        testInsertWhenFull();
+        testInsertOnAlreadyAvailableValue();
+        testPrepend();
+        testPopWhenValueAvailable();
+        testPopWhenValueNotAvailable();
     }
 
     private static void testGetLessThanZero() {
@@ -82,6 +119,77 @@ public class ResizeArray {
             System.out.println(resizeArray.get(resizeArray.size()));
         } catch (ArrayIndexOutOfBoundsException execption) {
             System.out.println("Array Index Out of Bounds");
+        }
+    }
+
+    public static void testInsertWhenNotFull() {
+        ResizeArray resizeArray = new ResizeArray();
+        resizeArray.insert(0, 1);
+        resizeArray.insert(1, 2);
+        resizeArray.insert(2, 3);
+        resizeArray.insert(3, 4);
+        resizeArray.insert(4, 5);
+        resizeArray.insert(5, 6);
+        for (int i = 0; i < resizeArray.size(); i++) {
+            System.out.println(resizeArray.get(i));
+        }
+    }
+
+    public static void testInsertWhenFull() {
+        ResizeArray resizeArray = new ResizeArray();
+        resizeArray.insert(0, 1);
+        resizeArray.insert(1, 2);
+        resizeArray.insert(2, 3);
+        assert resizeArray.capacity() == 4;
+        for (int i = 0; i < resizeArray.size(); i++) {
+            System.out.println(resizeArray.get(i));
+        }
+        System.out.println("Capcity is " + resizeArray.capacity());
+    }
+
+    public static void testInsertOnAlreadyAvailableValue() {
+        ResizeArray resizeArray = new ResizeArray();
+        resizeArray.insert(0, 1);
+        resizeArray.insert(1, 2);
+        resizeArray.insert(2, 3);
+        resizeArray.insert(2, 4);
+        resizeArray.insert(4, 5);
+        resizeArray.insert(5, 6);
+        for (int i = 0; i < resizeArray.size(); i++) {
+            System.out.println(resizeArray.get(i));
+        }
+    }
+
+    public static void testPrepend() {
+        ResizeArray resizeArray = new ResizeArray();
+        resizeArray.prepend(1);
+        resizeArray.prepend(2);
+        resizeArray.prepend(3);
+        resizeArray.prepend(4);
+        resizeArray.prepend(5);
+        resizeArray.prepend(6);
+        for (int i = 0; i < resizeArray.size(); i++) {
+            System.out.println(resizeArray.get(i));
+        }
+    }
+
+    public static void testPopWhenValueAvailable() {
+        ResizeArray resizeArray = new ResizeArray();
+        resizeArray.addLast(10);
+        resizeArray.insert(1, 8);
+        resizeArray.insert(2, 16);
+        resizeArray.insert(3, 9);
+        for (int i = 0; i < resizeArray.size(); i = 0) {
+            System.out.println(resizeArray.pop());
+        }
+    }
+
+    public static void testPopWhenValueNotAvailable() {
+        ResizeArray resizeArray = new ResizeArray();
+        try {
+            System.out.println(resizeArray.pop());
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
